27f03f58f5ec7e3d957938246b675d726ac88ff4
Nicholas Parise <1treasurehound@gmail.com>
2025-04-28 00:12:35 -0400

    updating projects

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import {Title} from '@angular/platform-browser'
 import { Project } from '../_models/Project';
 import { ProjectsService } from '../_services/projects.service';
@@ -7,7 +8,7 @@ import { Meta } from "@angular/platform-browser"
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
