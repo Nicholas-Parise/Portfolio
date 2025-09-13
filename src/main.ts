e5039d6dea0b746c1bd9c94ce389e5bcc0aae847
Nicholas Parise <1treasurehound@gmail.com>
2025-09-13 18:53:15 -0400

    updated to prerendered pages

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideClientHydration } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
-  
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideClientHydration(),
+    ...appConfig.providers,
+  ],
+}).catch(err => console.error(err));
