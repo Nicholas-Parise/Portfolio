eb503a67ba3f29c0557c11b924135d7b6e5e5db8
Nicholas Parise <1treasurehound@gmail.com>
2026-04-07 12:34:53 -0400

    route updates

---
+++ src/app/app.routes.server.ts
@@ -0,0 +1,29 @@
+import { RenderMode, ServerRoute } from '@angular/ssr';
+
+export const serverRoutes: ServerRoute[] = [
+
+    {
+        path: '',
+        renderMode: RenderMode.Prerender
+    },
+    {
+        path: 'home',
+        renderMode: RenderMode.Prerender
+    },
+    {
+        path: 'about',
+        renderMode: RenderMode.Prerender
+    },
+    {
+        path: 'projects',
+        renderMode: RenderMode.Prerender
+    },
+    {
+        path: 'resume',
+        renderMode: RenderMode.Prerender
+    },
+    {
+        path: 'games',
+        renderMode: RenderMode.Prerender
+    }
+];
